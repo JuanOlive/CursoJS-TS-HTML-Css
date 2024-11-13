b9df5d5db9b568824a03e1df90ef9d6e09c95666
JuanOlive <juanpyramids@gmail.com>
2024-11-13 16:28:10 -0300

    functions

---
+++ logicaProgramacao/aula.09.js
@@ -0,0 +1,13 @@
+
+
+function soma(item1, item2) {
+    return item1 + item2;
+
+}
+
+function diminuir(item1, item2){
+    return item1 - item2;
+
+}
+console.log('SOMA: ', soma(5, 6));
+console.log('SUBTRACAO: ', diminuir(34, 65));
