19764d7b3ee524f00f4c21fe192a06548c9c3b15
JuanOlive <juanpyramids@gmail.com>
2024-11-13 12:00:56 -0300

    if e else

---
+++ logicaProgramacao/aula-03.js
@@ -0,0 +1,11 @@
+const idade = 18;
+const nome ='julio';
+let podeEntrar = false;
+
+if( idade >= 18 && nome === 'julio') {
+    console.log('Pode entrar')
+} else {
+    console.log('NAOO ENTRA!!')
+}
+
+//console.log('usuário pode entrar', podeEntrar)
