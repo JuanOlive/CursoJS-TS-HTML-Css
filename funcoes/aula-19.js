72d837b4c2ef9982550bde2fc926c6bed479c9d2
JuanOlive <juanpyramids@gmail.com>
2024-11-18 16:19:17 -0300

    funcoes

---
+++ funcoes/aula-19.js
@@ -0,0 +1,15 @@
+function calcular () {
+
+    console.log('calcular')
+}
+
+const calcular2 = function() {
+    console.log('calcular 2');
+
+}
+
+const calcular3 = () => console.log("calcular 3");
+
+calcular();
+calcular2();
+calcular3();
