689b85cb4898aa986d66abd94a257a92885724f9
JuanOlive <juanpyramids@gmail.com>
2024-11-18 15:20:45 -0300

    manipulando strings

---
+++ logicaProgramacao/aula-14.js
@@ -0,0 +1,13 @@
+const mensagem = ' qualquer coisa ';
+const mensagem2 = ' dadasddadasd';
+
+console.log('comprimento: ', mensagem.length);
+console.log('concat: ', mensagem.concat(mensagem2));
+console.log('concatenacao +: ', " adsaad" + mensagem + mensagem2 );
+console.log('ES: ', `dsadas $(mensagem) $(mensagem)`);
+console.log("quebrar um array:", mensagem.split(' ') );
+
+console.log("up: ", mensagem.toUpperCase());
+console.log("down: ", mensagem.toLowerCase());
+console.log("replace: ", mensagem.replace('juan'));
+
