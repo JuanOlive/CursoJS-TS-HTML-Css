e8ae99946b53369b0590c19b9455464ba2eb208a
JuanOlive <juanpyramids@gmail.com>
2024-11-20 09:04:30 -0300

    soma recursiva

---
+++ funcoes/aula-24.js
@@ -0,0 +1,16 @@
+process.stdin.setEncoding('utf-8'); 
+ 
+ const soma = (valor) => {
+    if (valor === 0) {
+        return 0;
+    }
+    return valor + soma(valor -1)
+ }
+
+
+ process.stdin.on('data', function(data) {
+    console.log('soma: ', soma(Number(data)))
+ });
+
+
+ 
